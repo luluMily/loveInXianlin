f9dbf7458ef97fae6eb91a0d14f419452395706f
richthofen80 <richthofen80@localhost.localdomain>
2015-02-04 21:55:25 -0500

    2014-0204

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var crypto = require('crypto');
 var User = require('../models/user.js');
 var Post = require('../models/post.js');
+var formidable = require('formidable');
+var fs = require('fs');
 
 
 /* GET home page. */
@@ -64,6 +66,64 @@ router.post("/reg",function(req,res){
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: '登录' });
+});
+
+router.get('/upload', function(req, res, next) {
+  res.render('upload', { title: '上传' });
+});
+
+router.post('/upload', function(req, res) {
+
+  var form = new formidable.IncomingForm();   //创建上传表单
+      form.encoding = 'utf-8';        //设置编辑
+      form.uploadDir = 'public/uploads/';     //设置上传目录
+      form.keepExtensions = false;     //保留后缀
+      form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小
+
+    form.parse(req, function(err, fields, files) {
+
+        if (err) {
+          res.locals.error = err;
+          res.render(index, { title: TITLE });
+          return;        
+        }  
+
+/*       
+        var extName;  //后缀名
+        switch (files.fulAvatar.type) {
+            case 'image/pjpeg':
+                extName = 'jpg';
+                break;
+//            case 'image/jpeg':
+//                extName = jpg;
+//                break;         
+            case 'image/png':
+                extName = 'png';
+                break;
+            case 'image/x-png':
+                extName = 'png';
+                break;       
+        }
+
+//        if(extName.length == 0){
+//              res.locals.error = 只支持png和jpg格式图片;
+//              return res.redirect('/upload');                   
+//        }
+
+*/
+
+//        var avatarName = Math.random() + "." + extName;
+				var avatarName = Math.random();
+        var newPath = form.uploadDir + avatarName;
+
+        console.log(newPath);
+        fs.renameSync(files.fulAvatar.path, newPath);  //重命名
+    });
+
+//    req.flash('success','上传成功');
+//		res.render('upload', { title: '上传' });
+		return res.redirect('/upload');
+		       
 });
 
 
